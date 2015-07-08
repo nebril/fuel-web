4f22c70f57f36186e5877c6bcf3a548f7e943753
Sylwester Brzeczkowski <sbrzeczkowski@mirantis.com>
2015-07-08 16:29:18 +0200

    POC new format parser

--- nailgun/nailgun/extensions/volume_manager/manager.py
+++ nailgun/nailgun/extensions/volume_manager/manager.py
@@ -21,11 +21,15 @@ All sizes in megabytes.
 
 from copy import deepcopy
 from functools import partial
+import yaml
 
 from oslo.serialization import jsonutils
 
 from nailgun.errors import errors
 from nailgun.logger import logger
+
+from fuel_agent.drivers.nailgun import NailgunSimpleDriver
+from fuel_agent.objects.partition import PartitionScheme
 
 
 def is_service(space):
@@ -971,3 +975,149 @@ class VolumeManager(object):
 
     def __logger(self, message):
         logger.debug('VolumeManager %s: %s', id(self), message)
+
+
+class VolumesHumanFormatParser(object):
+
+    @classmethod
+    def yaml_from_path(cls, path):
+        with open(path) as f:
+            data = yaml.load(f)
+        return cls(data)
+
+    def __init__(self, data):
+        self._data = data
+        self.references = {}
+
+    @property
+    def json(self):
+        return jsonutils.dumps(self.parsed_dict)
+
+    @property
+    def yaml(self):
+        return yaml.dumps(self.parsed_dict)
+
+    @property
+    def partition_scheme(self):
+        if not hasattr(self, '_partition_scheme'):
+            self.parse()
+
+            p_scheme = PartitionScheme()
+            p_scheme.lvs = NailgunSimpleDriver.parse_lv_data(
+                self.parsed_dict['lvs'])
+            p_scheme.pvs = NailgunSimpleDriver.parse_pv_data(
+                self.parsed_dict['pvs'])
+            p_scheme.fss = NailgunSimpleDriver.parse_fs_data(
+                self.parsed_dict['fss'])
+            p_scheme.vgs = NailgunSimpleDriver.parse_vg_data(
+                self.parsed_dict['vgs'])
+            p_scheme.mds = NailgunSimpleDriver.parse_md_data(
+                self.parsed_dict['mds'])
+            p_scheme.parteds = \
+                NailgunSimpleDriver.parse_parted_data(
+                    self.parsed_dict['parteds'])
+
+            self._partition_scheme = p_scheme
+
+        return self._partition_scheme
+
+    def parse(self):
+        self.parsed_dict = {}
+        self.data = deepcopy(self._data)
+
+        self.parse_mds()
+        self.parse_parteds()
+        self.parse_vgs_and_pvs()
+        self.parse_lvs()
+        self.parse_fss()
+
+        return self.parsed_dict
+
+    def parse_mds(self):
+        mds = self.data.get('mds', [])
+
+        for md in mds:
+            md_id = md.pop('id')
+            self.references["@{0}/{1}".format('md', md_id)] = md
+
+        self.parsed_dict['mds'] = mds
+
+    def parse_parteds(self):
+        parteds = self.data.get('parteds', [])
+
+        for parted in parteds:
+            partitions = parted.pop('partitions', [])
+            parted_id = parted.pop('id')
+            parted['name'] = parted.pop('device')
+
+            parted['partitions'] = []
+            for partition in partitions:
+                tmp_partition = {}
+                device = partition.pop('device', None) or parted['name']
+
+                if device.startswith("@"):
+                    tmp_partition['device'] = self.references[device]['name']
+                else:
+                    tmp_partition['device'] = device
+
+                partition_id = partition.pop('id')
+                tmp_partition.update(partition)
+                parted['partitions'].append(tmp_partition)
+
+                self.references["@{0}/{1}/{2}".format(
+                    'parteds',
+                    parted_id,
+                    partition_id)
+                ] = tmp_partition
+
+        self.parsed_dict['parteds'] = parteds
+
+    def parse_vgs_and_pvs(self):
+        pvs = self.data.get('pvs', [])
+        for pv in pvs:
+            if pv['device'].startswith('@'):
+                device_ref = pv.pop('device')
+                pv['name'] = self.references[device_ref]['device']
+            else:
+                pv['name'] = pv.pop('device')
+
+        vgs = self.data.get('vgs', [])
+        self.parsed_dict['vgs'] = []
+        for vg in vgs:
+            tmp_vg = {}
+            tmp_vg['name'] = vg['name']
+            tmp_vg['pvnames'] = []
+            for pv_id in vg.get('pvs', []):
+                pv = next((elem for elem in pvs if elem['id'] == pv_id), None)
+                if pv is not None:
+                    tmp_vg['pvnames'].append(pv['name'])
+
+            self.parsed_dict['vgs'].append(tmp_vg)
+
+        # Removing ids from pvs
+        for pv in pvs:
+            pv.pop('id')
+
+        self.parsed_dict['pvs'] = pvs
+
+    def parse_lvs(self):
+        lvs = self.data.get('lvs', [])
+
+        for lv in lvs:
+            lv_id = lv.pop('id')
+            self.references["@{0}/{1}".format('lvs', lv_id)] = lv
+
+        self.parsed_dict['lvs'] = lvs
+
+    def parse_fss(self):
+        fss = self.data.get('fss', [])
+        for fs in fss:
+            if fs['device'].startswith('@'):
+                device_ref = fs.pop('device')
+                vg_name = self.references[device_ref]["vgname"]
+                lv_name = self.references[device_ref]["name"]
+                fs['device'] = "/dev/mapper/{0}-{1}".format(vg_name, lv_name)
+
+            fs.pop('id')
+
+        self.parsed_dict['fss'] = fss
